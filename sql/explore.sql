1783510a6e82cc4a7c283b3bcaab22b15ece681b
Rohit_Kosamkar <rohitkosamkar97@gmail.com>
2025-05-09 01:24:57 -0400

    sql script added

---
+++ sql/explore.sql
@@ -0,0 +1,14 @@
+select top 10 * from symptom_list
+
+select top 10 * from new_resulting_factors
+
+select top 10 * from user_info
+
+select * from new_resulting_factors as t1 ---where id in (29440)
+where user_input_id in ('6d3a2d37-6d0b-465e-aef6-a9d2ac6e2e23')
+
+
+
+select top 10 * from patient_therapies
+
+select top 10 * from symptom_list
